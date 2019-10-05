af566892d9c17e95240a7c96ab2430f8bdac1f6e
Duffsss <bruno_strano@hotmail.com>
2019-10-05 14:54:29 -0300

    New Components: Divider

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import Icon from './src/Icon';
 import {ICON_CLASS} from './src/Icon'
 import CardLine from './src/CardLine'
+import Divider from "./src/Divider";
 
-export {Icon,ICON_CLASS,CardLine};
+export {Icon,ICON_CLASS,CardLine,Divider};
---
+++ src/Divider.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+import PropTypes from 'prop-types';
+
+
+const Divider = (props) => {
+    return <View style={[styles.separator, {width: props.width}, props.style]}/>
+};
+export default Divider;
+
+const styles = StyleSheet.create({
+    separator: {
+        borderBottomColor: '#bbb',
+        borderBottomWidth: StyleSheet.hairlineWidth,
+    },
+});
+
+Divider.propTypes = {
+    style: PropTypes.object,
+    width: PropTypes.string
+};
+
+
+Divider.defaultProps = {
+    width: '100%',
+};
+
